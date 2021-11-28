0ad070075bc193d97df183a638d1b8c03d8ae9fb
Mekel Tensley <mekelintech@gmail.com>
2021-11-28 17:21:46 -0500

    Second commit

--- index.js
+++ index.js
@@ -15,10 +15,37 @@ console.log(person.foods);
 
 
 
-const somethingElse = require('./utils/myModule'); // imports the module 
+const somethingElse = require("./utils/myModule"); // imports the module 
 
 console.log(somethingElse); // this is just the object 
 
 somethingElse.hello(); // returns the string but doesn't print to terminal
 
 console.log(somethingElse.hello()); // returns and prints string 
+
+
+//using faker 
+
+const faker = require("faker");
+
+let randomName = faker.name.findName();
+console.log(randomName);
+
+const randomAddress = {
+    streetAddress: faker.address.streetAddress(),
+    city: faker.address.city(),
+    zipCode: faker.address.zipCode()
+}
+console.log(randomAddress);
+
+//importing axios 
+
+const axios = require('axios'); //allows to make API calls and retrieve data
+
+axios.get('https://api.github.com/users/mekeltensley')
+.then(function(response) {
+    console.log(response.data);
+})
+.catch(function(err) {
+    console.log(err);
+});
